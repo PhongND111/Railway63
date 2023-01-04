fa78274c563a1f7ae3662992455260a293e3944b
PhongND111 <lamourenfrance1@gmail.com>
2023-01-04 20:16:00 +0700

    Testing system 1

---
+++ Java/TestingSystem1/20230104/com/vti/entity/Account.java
@@ -0,0 +1,14 @@
+package com.vti.entity;
+
+import java.time.LocalDate;
+
+public class Account {
+	public int id;
+	public String email;
+	public String username;
+	public String fullName;
+	public int depID;
+	public int posID;
+	public LocalDate createDate;
+
+}
